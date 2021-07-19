68326981350792838638863ec2266305f3030384
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-20 00:20:23 +0530

    Add auth context file

---
+++ src/store/auth-context.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogIn: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const loggedInInfo = localStorage.getItem("isLoggedIn");
+    if (loggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
